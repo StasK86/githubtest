dac768f07967a219c41717253b772f8671615717
Stas <krigers@mail.ru>
2023-08-07 22:50:40 +0600

    java

--- src/test/java/githubtest21/TestSoft.java
+++ src/test/java/githubtest21/TestSoft.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class Githubtest {
+public class TestSoft {
 
     @Test
     void testSoftAssertions () {
