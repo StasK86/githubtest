219f42912280def5d5246dbd442d52aa3cd3be5f
Stas <krigers@mail.ru>
2023-08-07 22:39:12 +0600

    Java

---
+++ src/test/java/githubtest21/Githubtest.java
@@ -0,0 +1,34 @@
+package githubtest21;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Githubtest {
+
+    @Test
+    void testSoftAssertions () {
+        open("https://github.com/selenide/selenide");
+        $("ul.UnderlineNav-body a#wiki-tab").click();
+        $(".f6.Link--muted.js-wiki-more-pages-link.btn-link.mx-auto").click();
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(text("SoftAssertions"));
+        $("[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $(".markdown-body").$(byText("3. Using JUnit5 extend test class:")).sibling(0).shouldHave(text("""
+                     
+                     @ExtendWith({SoftAssertsExtension.class})
+                     class Tests {
+                          @Test
+                          void test() {
+                            Configuration.assertionMode = SOFT;
+                            open("page.html");
+                            $("#first").should(visible).click();
+                            $("#second").should(visible).click();
+                          }
+                    }
+                """));
+    }
+
+}
